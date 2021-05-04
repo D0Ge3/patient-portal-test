ce189d7083b6a61d22a6a677fb8baf273518ad62
Nikita <fnik340@gmail.com>
2021-05-04 19:23:55 +0300

    calendar

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,21 +1,74 @@
+import 'moment/locale/ru'
+import { useState, useEffect } from 'react'
+import moment from 'moment'
 import * as cs from 'classnames'
 
-import { Arrow } from '../../common/icons/Arrow'
+import { Arrow } from '../../common/icons/Arrow/Arrow'
 
 import s from './Calendar.module.scss'
 
 export const Calendar = () => {
-  const dateStyle = (isWhite) =>
+  const [date, setDate] = useState(moment())
+
+  const dateStyle = (isWhite, isGrey) =>
     cs({
       [s.date]: true,
       [s.date_white]: isWhite,
+      [s.date_grey]: isGrey,
     })
+  const ucFirst = (str) => {
+    if (!str) return str
+
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  const renderDatesArr = () => {
+    let days = []
+    let startOfMonth = moment(date).startOf('month')
+    let endOfMonth = moment(date).endOf('month')
+
+    for (let i = 0; i < +endOfMonth.format('D'); i++) {
+      days.push(moment(startOfMonth).add(i, 'd'))
+    }
+
+    if (days[0].weekday() !== 0) {
+      days = [...Array(days[0].weekday()).fill(null), ...days]
+    }
+    if (days[days.length - 1].weekday() !== 6) {
+      days = [...days, ...Array(6 - days[days.length - 1].weekday()).fill(null)]
+    }
+    return days.map((day, index) => (
+      <div
+        key={day ? day.format('x') : index}
+        className={dateStyle(
+          day !== null,
+          day !== null && moment(day).diff(moment(), 'days') < 0
+        )}
+      >
+        <span className={s.day}>{day ? day.format('D') : null}</span>
+        {index === 10 && (
+          <div className={s.event}>
+            <span>2</span>
+          </div>
+        )}
+      </div>
+    ))
+  }
+
   return (
     <div className={s.calendar}>
       <div className={s.header}>
-        <Arrow mode="back" />
-        <span>Июнь, 2020</span>
-        <Arrow mode="forward" />
+        <Arrow
+          mode="back"
+          onClick={() => setDate(moment(date).subtract(1, 'month'))}
+        />
+        <span>
+          {ucFirst(moment(date).format('MMMM'))}, {moment(date).year()}
+        </span>
+        <Arrow
+          mode="forward"
+          onClick={() => setDate(moment(date).add(1, 'month'))}
+        />
       </div>
       <div className={s.gridWrap}>
         <div className={s.gridLegend}>
@@ -27,20 +80,7 @@ export const Calendar = () => {
           <span>Сб</span>
           <span>Вс</span>
         </div>
-        <div className={s.dates}>
-          {Array(42)
-            .fill(null)
-            .map((item, index) => (
-              <div key={index} className={dateStyle(index > 0 && index < 32)}>
-                <span>{index}</span>
-                {index === 10 && (
-                  <div className={s.event}>
-                    <span>2</span>
-                  </div>
-                )}
-              </div>
-            ))}
-        </div>
+        <div className={s.dates}>{renderDatesArr()}</div>
       </div>
     </div>
   )
